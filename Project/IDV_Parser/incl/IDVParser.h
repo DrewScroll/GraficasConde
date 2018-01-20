2063e148d9f2687dcc4088162fd60a3be1a6b2f5
Andres Gonzalez Conde <andres.gonconde@gmail.com>
2018-01-19 20:00:50 -0600

    Modified Parser con Clase.

--- Project/IDV_Parser/incl/IDVParser.h
+++ Project/IDV_Parser/incl/IDVParser.h
@@ -1,14 +1,6 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-
-using namespace std;
-
 #ifndef IDV_PARSER
 #define IDV_PARSER
 
 void PrintFromLibrary();
-
-void Lectura(const char* NombreArchivo);
 
 #endif
